d5720cd2ef820c4cedfe010022e9d2c9fe0f2666
Júlio Monteiro <54995990+Julio-M39@users.noreply.github.com>
2022-07-20 19:42:33 -0300

    att final
    modulo imagem e vídeo

--- app.py
+++ app.py
@@ -16,13 +16,14 @@ from matplotlib import patches
 import time
 from skimage.feature import Cascade
 from PIL import Image
-from functions import extrai_face, yolo_person, blob_imagem, deteccoes, funcoes_imagem_v2, extrair_person
+from functions import detectFaceOpenCVDnn, detectFaceOpenCV, yolo_person, blob_imagem, deteccoes, extrair_person
 
 # ------ Detecção de rostos -------------
-# Carrega o arquivo de treino para o detector facial
-arquivo_treino = data.lbp_frontal_face_cascade_filename()
-# Criamos o detector
-detector = Cascade(arquivo_treino)
+modelFile = "cfg/res10_300x300_ssd_iter_140000.caffemodel"
+configFile = "cfg/deploy.prototxt.txt"
+net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
 # ------ Detecção de pessoas ------------
 
@@ -69,24 +70,24 @@ if app_mode == 'Sobre o aplicativo':
         """,
         unsafe_allow_html=True,
     )
-    st.video('https://www.youtube.com/watch?v=FMaNNXgB_5c&ab_channel=AugmentedStartups')
+    st.image('capa.png')
 
-    st.markdown('''
+    #st.markdown('''
           # About Me \n 
-            Hey this is ** Ritesh Kanjee ** from **Augmented Startups**. \n
+            #Hey this is ** Ritesh Kanjee ** from **Augmented Startups**. \n
 
-            If you are interested in building more Computer Vision apps like this one then visit the **Vision Store** at
-            www.augmentedstartups.info/visionstore \n
+            #If you are interested in building more Computer Vision apps like this one then visit the **Vision Store** at
+            #www.augmentedstartups.info/visionstore \n
 
-            Also check us out on Social Media
-            - [YouTube](https://augmentedstartups.info/YouTube)
-            - [LinkedIn](https://augmentedstartups.info/LinkedIn)
-            - [Facebook](https://augmentedstartups.info/Facebook)
-            - [Discord](https://augmentedstartups.info/Discord)
+            #Also check us out on Social Media
+            #- [YouTube](https://augmentedstartups.info/YouTube)
+            #- [LinkedIn](https://augmentedstartups.info/LinkedIn)
+            #- [Facebook](https://augmentedstartups.info/Facebook)
+            #- [Discord](https://augmentedstartups.info/Discord)
 
-            If you are feeling generous you can buy me a **cup of  coffee ** from [HERE](https://augmentedstartups.info/ByMeACoffee)
+            #If you are feeling generous you can buy me a **cup of  coffee ** from [HERE](https://augmentedstartups.info/ByMeACoffee)
 
-            ''')
+            #''')
 
 # Utilização do modo imagens
 elif app_mode == 'Imagens':
@@ -129,18 +130,18 @@ elif app_mode == 'Imagens':
             st.subheader('Faça o upload de uma imagem')
         else:
 
-            # Detecção de faces na imagem
-            faces_detectadas_1 = detector.detect_multi_scale(img=image,
-                                                             scale_factor=2.0,
-                                                             step_ratio=1,
-                                                             min_size=(5, 5),
-                                                             max_size=(300, 300))
+            imagens = detectFaceOpenCVDnn(net, image)
 
-            face = extrai_face(faces_detectadas_1, image)
+            faces = []
 
-            if len(face) > 0:
+            for i in range(len(imagens)):
+                img = imagens[i].shape
+                if img[0] > 5 and img[1] > 5:
+                    faces.append(imagens[i])
+
+            if len(faces) > 0:
                 st.subheader('Rostos encontrados')
-                st.image(face, use_column_width=150, width=150)
+                st.image(faces, use_column_width=100, width=100)
             else:
                 st.subheader('Nenhum rosto encontrado')
 
@@ -151,7 +152,7 @@ elif app_mode == 'Imagens':
             st.subheader('Faça o upload de uma imagem')
         else:
             net, ln, LABELS = yolo_person()
-            threshold = 0.5
+            threshold = 0.7
             threshold_NMS = 0.3
             classes = ['person']
 
@@ -173,11 +174,158 @@ elif app_mode == 'Imagens':
 
             pessoas = extrair_person(objs, caixas, LABELS, IDclasses, classes, imagem_cp)
 
-            if len(pessoas) > 0:
+            persons = []
+
+            for i in range(len(pessoas)):
+                img = pessoas[i].shape
+                if img[0] > 50 and img[1] > 50:
+                    persons.append(pessoas[i])
+
+            if len(persons) > 0:
                 st.subheader('Pessoas encontradas')
-                st.image(pessoas, use_column_width=150, width=150)
+                st.image(persons, use_column_width=100, width=100)
             else:
                 st.subheader('Nenhuma pessoa encontradas')
+
+elif app_mode =='Vídeos':
+
+    st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 400px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 400px;
+        margin-left: -400px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+        )
+
+    mode_video = st.sidebar.selectbox('Modos de detecção',
+                                      ['Escolher modos', 'Detecção de rostos', 'Detecção de pessoas']
+                                      )
+
+    stframe = st.empty()
+    video_file_buffer = st.sidebar.file_uploader("Carregue um vídeo", type=[ "mp4", "mov",'avi','asf', 'm4v' ])
+    tfflie = tempfile.NamedTemporaryFile(delete=False)
+
+    if video_file_buffer is not None:
+        tfflie.write(video_file_buffer.read())
+        vidcap = cv2.VideoCapture(tfflie.name)
+        st.sidebar.text('Vídeo original')
+        st.sidebar.video(tfflie.name)
+
+
+    # Detecção de rostos
+    if mode_video == 'Escolher modos':
+        st.subheader('Escolha o modo de detecção e faça o upload de um vídeo')
+
+    # Detecção de rostos
+    if mode_video == 'Detecção de rostos':
+        if video_file_buffer is None:
+            st.subheader('Faça o upload de um vídeo')
+        else:
+            frames = []
+            def getFrame(sec):
+                vidcap.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
+                hasFrames, image = vidcap.read()
+                if hasFrames:
+                    frames.append(image)
+                return hasFrames
+
+            sec = 0
+            frameRate = 2
+            success = getFrame(sec)
+
+            while success:
+                sec = sec + frameRate
+                sec = round(sec, 2)
+                success = getFrame(sec)
+
+            imags = detectFaceOpenCV(net, frames)
+
+            faces = []
+
+            for i in range(len(imags)):
+                img = imags[i].shape
+                if img[0] > 15 and img[1] > 15:
+                    faces.append(imags[i])
+
+            if len(faces) > 0:
+                st.subheader('Rostos encontrados')
+                st.image(faces, use_column_width=100, width=100)
+            else:
+                st.subheader('Nenhum rosto encontrado')
+
+    # Detecção de pessoas
+    if mode_video == 'Detecção de pessoas':
+        if video_file_buffer is None:
+            st.subheader('Faça o upload de um vídeo')
+        else:
+            frames = []
+            def getFrame(sec):
+                vidcap.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
+                hasFrames, image = vidcap.read()
+                if hasFrames:
+                    frames.append(image)
+                return hasFrames
+
+            sec = 0
+            frameRate = 2
+            success = getFrame(sec)
+
+            while success:
+                sec = sec + frameRate
+                sec = round(sec, 2)
+                success = getFrame(sec)
+
+            net, ln, LABELS = yolo_person()
+            threshold = 0.5
+            threshold_NMS = 0.3
+            classes = ['person']
+
+            pessoasimg = []
+
+            for i in range(len(frames)):
+                imagem = cv2.cvtColor(frames[i], cv2.COLOR_BGR2RGB)
+                (H, W) = imagem.shape[:2]
+
+                imagem_cp = imagem.copy()
+                net, imagem, layer_outputs = blob_imagem(net, imagem, ln)
+
+                caixas = []
+                confiancas = []
+                IDclasses = []
+
+                for output in layer_outputs:
+                    for detection in output:
+                        caixas, confiancas, IDclasses = deteccoes(detection, threshold, caixas, confiancas, IDclasses,
+                                                                  W, H)
+
+                objs = cv2.dnn.NMSBoxes(caixas, confiancas, threshold, threshold_NMS)
+
+                pessoas = extrair_person(objs, caixas, LABELS, IDclasses, classes, imagem_cp)
+
+                for i in range(len(pessoas)):
+                    pessoasimg.append(pessoas[i])
+
+            persons = []
+
+            for i in range(len(pessoasimg)):
+                img = pessoasimg[i].shape
+                if img[0] > 50 and img[1] > 50:
+                    persons.append(pessoasimg[i])
+
+            if len(persons) > 0:
+                st.subheader('Pessoas encontradas')
+                st.image(persons, use_column_width=150, width=150)
+            else:
+                st.subheader('Nenhuma pessoa encontradas')
+
+
 
 
 
--- functions.py
+++ functions.py
@@ -3,27 +3,37 @@ import cv2
 import numpy as np
 
 # Função para extrair as faces da imagem
-def extrai_face(faces_detectadas_1, image):
-    face_face = []
-    for i in range(len(faces_detectadas_1)):
-        d = faces_detectadas_1[i]
+def detectFaceOpenCVDnn(net, frame, conf_threshold=0.7):
+    faces = []
 
-        # Aqui, x e y representam os pontos das coordenadas da área da face detectada
-        x, y = d['r'], d['c']
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+    blob = cv2.dnn.blobFromImage(frame, 1.8, (800, 800), [104, 117, 123], True, False, )
 
-        # Largura e altura da área da face detectada
-        width, height = d['r'] + d['width'], d['c'] + d['height']
+    net.setInput(blob)
+    detections = net.forward()
 
-        # Extraímos as faces detectadas
-        face = image[x:width, y:height]
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
 
-        face_face.append(face)
+            top = x1
+            right = y1
+            bottom = x2 - x1
+            left = y2 - y1
 
-    return face_face
+            # Extraímos as faces detectadas
+            face = frame[right:right + left, top:top + bottom]
+            faces.append(face)
+
+    return faces
 
 # --------------------------------------------------------------------------------
-# Detecção de pessoas com YOLO
-
+# Detecção de pessoas com YOLO em  imagens
 def yolo_person():
     labels_path = 'cfg/coco.names'
     LABELS = open(labels_path).read().strip().split('\n')
@@ -79,4 +89,35 @@ def extrair_person(objs, caixas, LABELS, IDclasses, classes, imagem_cp):
     return persons
 
 
+def detectFaceOpenCV(net, frames, conf_threshold=0.7):
+    faces = []
+
+    for i in range(len(frames)):
+
+        frame = cv2.cvtColor(frames[i], cv2.COLOR_BGR2RGB)
+        frameHeight = frame.shape[0]
+        frameWidth = frame.shape[1]
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (800, 800), [104, 117, 123], False, False, )
+
+        net.setInput(blob)
+        detections = net.forward()
+
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > conf_threshold:
+                x1 = int(detections[0, 0, i, 3] * frameWidth)
+                y1 = int(detections[0, 0, i, 4] * frameHeight)
+                x2 = int(detections[0, 0, i, 5] * frameWidth)
+                y2 = int(detections[0, 0, i, 6] * frameHeight)
+
+                top = x1
+                right = y1
+                bottom = x2 - x1
+                left = y2 - y1
+
+                # Extraímos as faces detectadas
+                face = frame[right:right + left, top:top + bottom]
+                faces.append(face)
+
+    return faces
 
